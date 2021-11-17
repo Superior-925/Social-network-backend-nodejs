6223513582e67f6333ed4376cad2dddc02e0ffd4
Anton Logunov <skykeeper925@gmail.com>
2021-11-17 08:25:41 +0300

    Changes for heroku

--- src/index.js
+++ src/index.js
@@ -106,11 +106,6 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.options('*', cors());
-app.all('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-});
 app.use(express.json());
 
 app.get('/', (req, res) => {
